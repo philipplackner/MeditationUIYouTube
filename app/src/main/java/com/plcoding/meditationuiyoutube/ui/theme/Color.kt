00420aa09f9ed0e3d07c779644694d38e5d63290
Philipp <mike.litoris@gmail.com>
2021-06-15 17:43:10 +0200

    Initial commit

---
+++ app/src/main/java/com/plcoding/meditationuiyoutube/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.plcoding.meditationuiyoutube.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val TextWhite = Color(0xffeeeeee)
+val DeepBlue = Color(0xff06164c)
+val ButtonBlue = Color(0xff505cf3)
+val DarkerButtonBlue = Color(0xff566894)
+val LightRed = Color(0xfffc879a)
+val AquaBlue = Color(0xff9aa5c4)
+val OrangeYellow1 = Color(0xfff0bd28)
+val OrangeYellow2 = Color(0xfff1c746)
+val OrangeYellow3 = Color(0xfff4cf65)
+val Beige1 = Color(0xfffdbda1)
+val Beige2 = Color(0xfffcaf90)
+val Beige3 = Color(0xfff9a27b)
+val LightGreen1 = Color(0xff54e1b6)
+val LightGreen2 = Color(0xff36ddab)
+val LightGreen3 = Color(0xff11d79b)
+val BlueViolet1 = Color(0xffaeb4fd)
+val BlueViolet2 = Color(0xff9fa5fe)
+val BlueViolet3 = Color(0xff8f98fd)
