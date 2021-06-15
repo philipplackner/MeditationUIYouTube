48426ade79018d98c8726cccc13d66341be96d5c
Philipp <mike.litoris@gmail.com>
2021-06-15 18:00:41 +0200

    Initial commit

--- app/src/main/java/com/plcoding/meditationuiyoutube/ui/theme/Type.kt
+++ app/src/main/java/com/plcoding/meditationuiyoutube/ui/theme/Type.kt
@@ -2,27 +2,40 @@ package com.plcoding.meditationuiyoutube.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.plcoding.meditationuiyoutube.R
+
+val gothicA1 = FontFamily(
+    listOf(
+        Font(R.font.gothica1_regular, FontWeight.Normal),
+        Font(R.font.gothica1_medium, FontWeight.Medium),
+        Font(R.font.gothica1_semibold, FontWeight.SemiBold),
+        Font(R.font.gothica1_bold, FontWeight.Bold),
+        Font(R.font.gothica1_black, FontWeight.Black),
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        color = AquaBlue,
+        fontFamily = gothicA1,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
         fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+    h1 = TextStyle(
+        color = TextWhite,
+        fontFamily = gothicA1,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
+    ),
+    h2 = TextStyle(
+        color = TextWhite,
+        fontFamily = gothicA1,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
     )
-    */
 )
